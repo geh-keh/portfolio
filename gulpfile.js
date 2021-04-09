72ca429b8ffbc8446a713e66238c8be5af553452
keh <q8127zoz@yahoo.co.jp>
2021-04-09 10:30:05 +0900

    ・gulpタスクの追加 ・Sass導入→Sassファイル新規作成 ・html mainVisualと introductionのイメージ画width設定

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var sassGlob = require("gulp-sass-glob");
+var sourcemaps = require("gulp-sourcemaps");
+var plumber = require("gulp-plumber");
+var notify = require("gulp-notify");
 
-gulp.task('sass', function() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest('./css'));
+gulp.task("sass", function () {
+  return gulp
+    .src("./sass/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./css"));
 });
 
-gulp.task('sass:watch', function() {
-  gulp.watch('./sass/**/*.scss', gulp.task('sass'));
+gulp.task("sass:watch", function () {
+  gulp.watch("./sass/**/*.scss", gulp.task("sass"));
 });
 
 // 初版・2刷の方へ
